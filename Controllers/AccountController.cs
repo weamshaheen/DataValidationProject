af69d03bc9c5596aa8ac12bd68cbe8d568bbbf87
weamshaheen <FOR LAPTOP@DESKTOP-RB4SBOD>
2025-04-17 15:50:58 +0300

    Login Form

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using DataValidationProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataValidationProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        //GET : /Account/Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        //PPST : /Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(UserLogin model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return RedirectToAction("LoginSuccess");
+        }
+
+        //GET : /Account/LoginSuccess
+        [HttpGet]
+        public IActionResult LoginSuccess()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/UserLogin.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataValidationProject.Models
+{
+    public class UserLogin
+    {
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Username is rquired")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Username must be between 3 and 50 character") ]
+        public string Username { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Password is rquired")]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Username must at least 6 characters ")] 
+
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        [Required(ErrorMessage = "Remember me is rquired")]
+        public bool RememberMe { get; set; }
+
+    }
+}
+ 
